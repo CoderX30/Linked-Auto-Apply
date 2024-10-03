73712dc03e00a09f46a1f6900f54a74a6712c51c
Rishit Kumar <95247997+CoderX30@users.noreply.github.com>
2024-10-03 23:28:02 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -31,7 +31,7 @@ def automation(linkedin_username, linkedin_password, search_query):
         options.add_argument("--disable-gpu")
         options.add_argument("--start-maximized")
         # options.add_argument("--disable-gpu")
-        # options.add_argument("--headless")  # Run in headless mode to avoid opening a browser
+        options.add_argument("--headless")  # Run in headless mode to avoid opening a browser
         driver = webdriver.Chrome(
         service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
         options=options,
