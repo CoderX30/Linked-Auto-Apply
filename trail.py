281e84193e24e3d19f3d56c43c6b48a7cd5c6514
Rishit Kumar <95247997+CoderX30@users.noreply.github.com>
2024-10-03 19:11:37 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -18,7 +18,7 @@ def automation(linkedin_username, linkedin_password, search_query):
 
     # Initialize the Chrome driver
 
-    driver_path = 'chromedriver.exe'  # Replace with your WebDriver path
+    driver_path = './chromedriver.exe'  # Replace with your WebDriver path
     service = Service(driver_path)
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")  # Start browser maximized
